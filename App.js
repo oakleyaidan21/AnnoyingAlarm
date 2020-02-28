a9349a46a071d25acb0ecafb57a19777aca0a8b0
Aidan Oakley <oakleyaidan@gmail.com>
2020-02-28 13:52:41 -0800

    added top header

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from "react-native";
+import { Header } from "react-native-elements";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Header
+        containerStyle={{ backgroundColor: "#ededed" }}
+        centerComponent={{
+          text: "ALARMS",
+          style: { color: "orange" }
+        }}
+        rightComponent={{ icon: "add", color: "orange" }}
+      />
+      <View style={styles.container}>
+        <ScrollView style={{ flex: 1, width: "100%" }}>
+          {/* Where the alarms will go */}
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
